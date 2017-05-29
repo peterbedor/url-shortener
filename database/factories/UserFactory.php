01f3f0930e229a746a3ebcf39743acfd996ae050
Peter <peter@lewiscommunications.com>
2017-05-29 17:13:11 -0500

    Add user model factory

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Models\User;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(User::class, function(Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
